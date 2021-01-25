c46b97b42174e1872f2b43035c7833754d7e5114
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-01-25 07:38:52 -0500

    Obtener el ultimo valor del stream

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -23,6 +23,10 @@ class LoginBloc with Validators {
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
+  //
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
+
   dispose() {
     _emailController?.close();
     _passwordController?.close();
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:form_validation/src/bloc/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home Page'),
+    final bloc = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HOME'),
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Email: ${bloc.email}'),
+            Divider(),
+            Text('Home ${bloc.password}'),
+          ],
+        ),
+      ),
     );
   }
 }
