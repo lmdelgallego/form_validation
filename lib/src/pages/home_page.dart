0d9c77e5ad6285ee6d1071024760e33d29180dba
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-01-29 07:45:59 -0500

    MOstrar listado de productos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -31,11 +31,33 @@ class HomePage extends StatelessWidget {
       builder:
           (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
         if (snapshot.hasData) {
-          return Container();
+          final productos = snapshot.data;
+          return ListView.builder(
+            itemCount: productos.length,
+            itemBuilder: (BuildContext context, int index) =>
+                _crearItem(context, productos[index]),
+          );
         } else {
           return Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+
+  Widget _crearItem(BuildContext context, ProductoModel producto) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red,
+      ),
+      onDismissed: (direction) {
+        print(direction);
+      },
+      child: ListTile(
+        title: Text('${producto.titulo} - ${producto.precio} '),
+        subtitle: Text(producto.id),
+        onTap: () => Navigator.pushNamed(context, 'producto'),
+      ),
     );
   }
 }
