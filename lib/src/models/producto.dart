d5ee9e5893432d29c5f8db135207291399502acf
Luis Miguel Del Gallego <luis.delgallego@avaldigitallabs.com>
2021-01-29 06:51:51 -0500

    Product model

---
+++ lib/src/models/producto.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+ProductoModel productoModelFromJson(String str) =>
+    ProductoModel.fromJson(json.decode(str));
+
+String productoModelToJson(ProductoModel data) => json.encode(data.toJson());
+
+class ProductoModel {
+  ProductoModel({
+    this.id,
+    this.titulo = '',
+    this.precio = 0.0,
+    this.disponible = true,
+    this.fotoUrl,
+  });
+
+  String id;
+  String titulo;
+  double precio;
+  bool disponible;
+  String fotoUrl;
+
+  factory ProductoModel.fromJson(Map<String, dynamic> json) => ProductoModel(
+        id: json["id"],
+        titulo: json["titulo"],
+        precio: json["precio"],
+        disponible: json["disponible"],
+        fotoUrl: json["fotoUrl"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "titulo": titulo,
+        "precio": precio,
+        "disponible": disponible,
+        "fotoUrl": fotoUrl,
+      };
+}
